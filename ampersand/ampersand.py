19316483b4968be1e0b0f908ee496469a90ffbc5
natejms <natejms@outlook.com>
2017-05-20 13:15:29 -0300

    Removed necessity of 'path' in _config.json

--- ampersand/ampersand.py
+++ ampersand/ampersand.py
@@ -40,14 +40,6 @@ def call_for_help():
     print("             compile - Compiles the specified modal")
     print("               serve - Compiles all modals\n")
 
-def is_ampersand(config):
-    if config["path"] in p.abspath("./"):
-        return True
-    else:
-        print("This folder doesn't seem to be a valid Ampersand site.\nTry " +
-            "running this command again from the root of the project.")
-        return False
-
 def build_file(modal, new_file, content):
     # Render the template HTML file
     new_content = pystache.render(read_file(modal), content)
@@ -57,25 +49,25 @@ def build_file(modal, new_file, content):
     generated.write(HTMLParser().unescape(new_content))
     generated.close()
 
-def translate_file(file_name, config):
-    layout_files = os.listdir(p.join(config["path"], config["layouts"]))
+def translate_file(file_name, config, root):
+    layout_files = os.listdir(p.join(root, config["layouts"]))
     layouts = {}
     for i in range(len(layout_files)):
-        contents = read_file(p.join(config["path"], config["layouts"], layout_files[i]))
+        contents = read_file(p.join(root, config["layouts"], layout_files[i]))
         layouts[p.splitext(layout_files[i])[0]] = contents
 
     # Create variables pointing to items in the configuration
     template = config["files"][file_name]
-    template_path = p.join(config["path"], "_modals", file_name)
+    template_path = p.join(root, "_modals", file_name)
     translation = config["files"][file_name]
-    build_dir = p.join(config["path"], config["site"])
+    build_dir = p.join(root, config["site"])
 
     for key, value in sorted(template.items()):
-        trans = get_json(p.join(config["path"], config["files"][file_name][key]))
+        trans = get_json(p.join(root, config["files"][file_name][key]))
 
         if key != config["primary"]:
-            if not p.exists(p.join(config["path"], config["site"], key)):
-                os.mkdir(p.join(config["path"], config["site"], key))
+            if not p.exists(p.join(root, config["site"], key)):
+                os.mkdir(p.join(root, config["site"], key))
 
         print(" * Translating '%s' in '%s'" % (template_path, key))
 
@@ -84,34 +76,42 @@ def translate_file(file_name, config):
         else: build_path = file_name
 
         build_file(template_path,
-            p.join(config["path"], config["site"], build_path),
+            p.join(root, config["site"], build_path),
             {"trans": trans, "layouts": layouts, "config": config})
+
+try:
+    config = get_json("_config.json")
+    root = os.listdir("_config.json")
+except FileNotFoundError:
+    try:
+        location = input("Enter the path (from here) to the root of your project: ")
+        config = get_json(p.join(location, "_config.json"))
+        root = p.abspath(location)
+    except (KeyboardInterrupt, FileNotFoundError) as e:
+        print(str(e))
+        sys.exit()
 
 def ampersand():
     if len(args) > 1:
-        if args[1] != "new":
-            config = open_config()
-            if args[1] == "compile":
+        if args[1] == "compile":
 
-                print("Compiling page '%s'" % (args[2]))
+            print("Compiling page '%s'" % (args[2]))
 
-                # Iterate through the translations and insert the layouts
-                try:
-                    translate_file(args[2], config)
-                except KeyError as e:
-                    print("Didn't recognize %s as a file in _config.json" % args[2])
-                    sys.exit()
-            elif args[1] == "serve":
+            # Iterate through the translations and insert the layouts
+            try:
+                translate_file(args[2], config, root)
+            except KeyError as e:
+                print("Didn't recognize %s as a file in _config.json" % args[2])
+                sys.exit()
+        elif args[1] == "serve":
 
-                print("Compiling all pages")
-                files = config["files"]
-                for key, value in sorted(files.items()):
-                    translate_file(key, config)
+            print("Compiling all pages")
+            files = config["files"]
+            for key, value in sorted(files.items()):
+                translate_file(key, config, root)
         else:
             if len(args) > 2:
                 print("Creating new site '%s'" % (args[2]))
-
-                path = p.abspath(args[2])
                 lang = "en"
                 if len(args) > 3:
                     lang = args[3]
@@ -132,8 +132,7 @@ def ampersand():
                 abspath = p.dirname(p.abspath(__file__))
                 build_file(p.join(abspath, "templates/_config.json"), args[2] + "/_config.json", {
                     "name": args[2],
-                    "lang": lang,
-                    "path": path
+                    "lang": lang
                 })
                 print("Created boilerplate website.")
             else:

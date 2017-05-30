be4f61c9467b55637535c54411f0a64bd000f640
natejms <natejms@outlook.com>
2017-05-30 16:28:39 -0300

    Moving to 0.3.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def readme():
 
 setup(
     name = "ampersand",
-    version = "0.2.0",
+    version = "0.3.0",
     description = "A really, really minimalistic static site generator",
     long_description = readme(),
     classifiers = [

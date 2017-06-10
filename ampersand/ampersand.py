e7f8d66158509cb0149aa64937b7486def7567dd
natejms <natejms@outlook.com>
2017-06-10 20:48:11 -0300

    Added comments, improved readability

--- ampersand/ampersand.py
+++ ampersand/ampersand.py
@@ -4,79 +4,110 @@ import sys, os, json, subprocess, importlib, inspect
 p = os.path
 
 class Ampersand(object):
+
     """docstring for Ampersand."""
+
     def __init__(self):
 
+        # Attempt to find the _ampersand.json configuration file
         try:
             config = build.get_json("_ampersand.json")
             root = p.dirname(p.abspath("./_ampersand.json"))
+
         except FileNotFoundError:
+            # Ask the user where to find the _ampersand.json file
+
             try:
-                location = input("Enter the path (from here) to the root of your project: ")
+                location = input("Enter the path (from here) to the root of "
+                                + "your project: ")
                 config = build.get_json(p.join(location, "_ampersand.json"))
                 root = p.abspath(location)
-            except (KeyboardInterrupt, FileNotFoundError, NotADirectoryError) as e:
+
+            except (KeyboardInterrupt,
+                    FileNotFoundError,
+                    NotADirectoryError) as e:
+
                 print(str(e))
                 sys.exit()
 
         self.root = root
         self.config = config
 
-
     def serve(self):
+
         print("Compiling all pages")
+
+        # Create an empty dictionary to store each pages content
         pages = {}
         for key, value in sorted(self.config["files"].items()):
-            print(key)
+            # Iterate through the files to build the pages dictionary
             pages[key] = build.collect(key, self)
 
+        # Build the pages
         build.build_pages(pages, self)
 
     def compile(self, filename):
+
         print("Compiling page '%s'" % filename)
-        # Iterate through the translations and insert the layouts
+        # Try to build the specified page
         try:
             build.build_pages({filename: build.collect(filename, self)}, self)
+
         except KeyError:
+            # Exit if not found in _ampersand.json
             print("Didn't recognize %s as a file in _ampersand.json" % filename)
             sys.exit()
 
     def plugin_add(self, url):
+
         try:
+            # Decide on what to call the plugin and its path
             plugin = p.splitext(p.split(url)[1])[0]
             plugin_path = p.join(self.root, self.config["modules"], plugin)
-            print("Installing Ampersand plugin '%s'" % plugin)
 
+            # Download the plugin via git
+            print("Installing Ampersand plugin '%s'" % plugin)
             subprocess.call(["git", "clone", url, plugin_path])
 
-            self.config["plugins"][p.basename(plugin)] = p.join(self.config["modules"], plugin)
+            # Update the _ampersand.json file by adding the plugin
+            self.config["plugins"][p.basename(plugin)] = p.join(
+                self.config["modules"], plugin )
             updated = open(p.join(self.root, "_ampersand.json"), "w")
             updated.write(json.dumps(self.config, indent=4))
             updated.close()
+
         except KeyError as e:
             print("Missing entry in your configuration file: %s" % str(e))
 
     def plugin_remove(self, name):
-        try:
-            print("Removing plugin '%s'" % name)
-            rmtree(p.join(self.root, self.config["plugins"][name]))
 
+        try:
+            # Delete the directory containing the plugin
+            print("Removing plugin '%s'" % name)
+            rmtree(p.join( self.root, self.config[ "plugins" ][ name ] ))
+
+            # Update _ampersand.json by adding the plugin
             self.config["plugins"].pop(name)
             updated = open(p.join(self.root, "_ampersand.json"), "w")
             updated.write(json.dumps(self.config, indent=4))
             updated.close()
+
         except KeyError:
             print("Failed to remove plugin '%s' as it is not installed." % name)
             sys.exit()
 
     def plugin_run(self, name, content):
+
         try:
-            plugin = build.get_json(p.join(self.root, self.config["plugins"][name], "_plugin.json"))
-            sys.path.append(p.join(self.root, self.config["plugins"][name]))
-            print(p.join(self.root, self.config["plugins"][name]))
+            # Retrieve the _plugin.json file
+            plugin = build.get_json(
+                p.join(self.root, self.config["plugins"][name], "_plugin.json"))
+
+            # Load and run the module
             module = importlib.import_module("main", name)
             content = module.main(content)
             return content
+
         except (KeyError, FileNotFoundError) as e:
             print("Failed to run plugin '%s': %s" % (name, e))
             return content
--- ampersand/build.py
+++ ampersand/build.py
@@ -2,17 +2,19 @@ import sys, os, json, pystache
 p = os.path # Aliasing os.path to 'p'
 
 def read_file(path):
+    # Open a file and return its contents
     f = open(path, "r")
     content = f.read()
     f.close()
     return content
 
 def get_json(path):
+    # Load a JSON file into a dictionary
     try:
         return json.loads(read_file(path))
+
     except json.decoder.JSONDecodeError as e:
-        print("It seems like you have an error in your JSON file.\n" +
-            "Check that and try again.")
+        print("It seems like you have an error in your JSON file. Check that and try again.")
         print(str(e))
         sys.exit()
 
@@ -38,57 +40,68 @@ def collect(file_name, site):
     pages = {}
 
     for key, value in sorted(template.items()):
+        # Read the selected file's translation into the trans variable
         try:
             trans = get_json(p.join(root, config["files"][file_name][key]))
+
         except FileNotFoundError as e:
             print(str(e))
             sys.exit()
 
+        # Read _global.json into _globale
         try:
             _global = get_json(p.join(root, config["translations"], key, "_global.json"))
+
         except FileNotFoundError:
             _global = {}
+
 
         layout_files = os.listdir(p.join(root, config["layouts"]))
         layouts = {}
         for i in range(len(layout_files)):
+            # Read the layout into "contents"
             contents = read_file(p.join(root, config["layouts"], layout_files[i]))
+            # Render the layouts using _ampersand.json and _global.json
             layouts[p.splitext(layout_files[i])[0]] = pystache.render(
                 contents, {"config": config, "global": _global})
 
+        # Read any external file translations into the dictionary
         for t_key, t_value in sorted(trans.items()):
             if t_value.startswith("file:"):
                 trans[t_key] = read_file(
                     p.join(root, p.dirname(translation[key]),
                     t_value.replace("file:", "")))
 
+        # Assign the collected contents to the pages dictionary
         print(" * Translating '%s' in '%s'" % (template_path, key))
-
-        # Build the translation
-        if key != config["primary"]: build_path = p.join(key, file_name)
-        else: build_path = file_name
-
         content = {"trans": trans, "layouts": layouts, "config": config, "global": _global}
         for key, value in sorted(site.config["plugins"].items()):
             content = site.plugin_run(key, content)
 
         pages[key] = content
+
     return pages
 
 def build_pages(content, site):
     config = site.config
     root = site.root
 
+    # Iterate through the plugins
     for key, value in sorted(config["plugins"].items()):
         site.plugin_run(key, content)
 
+    # Iterate through the files
     for key, value in sorted(content.items()):
+        # Iterate through the translations
         for k, v in sorted(content[key].items()):
+            # Set up the tree
             if k != config["primary"]:
                 if not p.exists(p.join(root, config["site"], key)):
                     os.mkdir(p.join(root, config["site"], k))
 
             if k != config["primary"]: build_path = p.join(k, key)
             else: build_path = key
+
+            # Build the file
             build_file(config["files"][key][k],
                 p.join(site.root, site.config["site"], build_path), content)
